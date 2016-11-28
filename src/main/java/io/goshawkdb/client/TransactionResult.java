e35764359cba8f09506769790ba956b3da979e7d
Matthew Sackman <matthew@goshawkdb.io>
2016-11-28 15:13:47 +0000

    Introduce getResultOrRethrow and getResultOrAbort in TxnResult. Ref T62.
    --HG--
    branch : T62

--- src/main/java/io/goshawkdb/client/TransactionResult.java
+++ src/main/java/io/goshawkdb/client/TransactionResult.java
@@ -32,4 +32,50 @@ public class TransactionResult<R> {
     public boolean isAborted() {
         return cause == TransactionAbortedException.e || (cause instanceof TransactionAbortedException);
     }
+
+    /**
+     * Returns the result of the transaction (which may be null) unless an exception occurred, in
+     * which case it re-throws the exception. Within a transaction, you want to use getResultOrAbort
+     * instead.
+     *
+     * @return The result of the transaction
+     * @throws Exception if the transaction itself threw an exception
+     */
+    public R getResultOrRethrow() throws Exception {
+        if (cause == null) {
+            return result;
+        } else {
+            throw cause;
+        }
+    }
+
+    /**
+     * Returns the result of the transaction (which may be null) unless an exception occurred, in
+     * which case it throws a {@link TransactionAbortedException} wrapping the exception. When
+     * within a transaction, use this method to inspect the result of nested transactions.
+     *
+     * @return The result of the transaction
+     * @throws TransactionAbortedException if the transaction itself threw an exception
+     */
+    public R getResultOrAbort() throws TransactionAbortedException {
+        if (cause == null) {
+            return result;
+        } else if (isAborted()) {
+            throw (TransactionAbortedException) cause;
+        } else {
+            throw new TransactionAbortedException(cause);
+        }
+    }
+
+    /**
+     * If you want to modify the result of a transaction, use this method. It keeps the txnid.
+     *
+     * @param r   The new result (can be null)
+     * @param e   The new exception (can be null)
+     * @param <S> The type of the new result
+     * @return A new TransactionResult with the same txnid as the current.
+     */
+    public <S> TransactionResult<S> extend(S r, Exception e) {
+        return new TransactionResult<>(r, txnid, e);
+    }
 }
